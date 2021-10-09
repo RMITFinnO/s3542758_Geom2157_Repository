728ff60dffd65b9d741c3ae60b69a5296674a835
RMITFinnO <s3542758@student.rmit.edu.au>
2021-10-09 16:08:07 +1100

    Add SUITABILITY attribute to clipped layer

--- Fresh.py
+++ Fresh.py
@@ -1,10 +1,20 @@
+import processing
+#import variables
 filepath = "D:\\Uni\\2021Semester2\\Geospatial_Programming\\Final_Project_Dataset\\Order_CJPSZB\\mga2020_55\\esrishape\\vicgov_region\\GIPPSLAND-0\\FIRE\\"
 firehistory = "FIRE_HISTORY.shp"
 siteborder = "siteborder.shp"
-
+#import fire history and site border
 fhistoryLayer = iface.addVectorLayer(filepath + firehistory, firehistory[:-4], "ogr")
 siteborder = iface.addVectorLayer(filepath + siteborder, siteborder[:-4], "ogr")
+#define fireborder clip dictionary
 fireborderDict = {'INPUT':fhistoryLayer, 'OVERLAY':siteborder, 'OUTPUT':filepath + 'fireborder.shp'}
-
+#clip firehistory layer to site border
 fireborderClip = processing.run("native:clip", fireborderDict)
-fireborderLayer = iface.addVectorLayer(filepath + 'fireborder.shp', "fireborderLayer", "ogr")
+fireborderLayer = iface.addVectorLayer(filepath + 'fireborder.shp', "", "ogr")
+
+fireborderLayer.startEditing()
+fireborderLayer.addAttribute(QgsField('SUITABILITY', QVariant.String))
+fireborderLayer.updateFields()
+fireborderLayer.commitChanges()
+
+
