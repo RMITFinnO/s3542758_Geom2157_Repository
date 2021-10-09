b07f68cd2c6a7450d718f2374b399b0891f2e0c4
RMITFinnO <s3542758@student.rmit.edu.au>
2021-10-09 18:39:12 +1100

    Extract 2021 layer

--- Fresh.py
+++ Fresh.py
@@ -3,6 +3,7 @@ import processing
 filepath = "D:\\Uni\\2021Semester2\\Geospatial_Programming\\Final_Project_Dataset\\Order_CJPSZB\\mga2020_55\\esrishape\\vicgov_region\\GIPPSLAND-0\\FIRE\\"
 firehistory = "FIRE_HISTORY.shp"
 siteborder = "siteborder.shp"
+
 #import fire history and site border
 fhistoryLayer = iface.addVectorLayer(filepath + firehistory, firehistory[:-4], "ogr")
 siteborder = iface.addVectorLayer(filepath + siteborder, siteborder[:-4], "ogr")
@@ -17,4 +18,7 @@ fireborderLayer.addAttribute(QgsField('SUITABILITY', QVariant.String))
 fireborderLayer.updateFields()
 fireborderLayer.commitChanges()
 
-
+currentYear = 2021
+extractDict = {'INPUT':fireborderLayer, 'FIELD':'SEASON', 'OPERATOR':0, 'VALUE':str(currentYear), 'OUTPUT':filepath + str(currentYear) + ".shp"}
+Testprocess = processing.run("native:extractbyattribute", extractDict)
+CurrentYearLayer = iface.addVectorLayer(filepath + str(currentYear) + ".shp", str(currentYear), "ogr")
