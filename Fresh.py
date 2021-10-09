420a904f9d7a6f41d38bb20588fa77666afdfbb9
RMITFinnO <s3542758@student.rmit.edu.au>
2021-10-09 14:04:07 +1100

    Clip fire history layer to site boundary

--- Fresh.py
+++ Fresh.py
@@ -1,8 +1,10 @@
-#establish variables
 filepath = "D:\\Uni\\2021Semester2\\Geospatial_Programming\\Final_Project_Dataset\\Order_CJPSZB\\mga2020_55\\esrishape\\vicgov_region\\GIPPSLAND-0\\FIRE\\"
 firehistory = "FIRE_HISTORY.shp"
 siteborder = "siteborder.shp"
 
-#import layers
 fhistoryLayer = iface.addVectorLayer(filepath + firehistory, firehistory[:-4], "ogr")
 siteborder = iface.addVectorLayer(filepath + siteborder, siteborder[:-4], "ogr")
+fireborderDict = {'INPUT':fhistoryLayer, 'OVERLAY':siteborder, 'OUTPUT':filepath + 'fireborder.shp'}
+
+fireborderClip = processing.run("native:clip", fireborderDict)
+fireborderLayer = iface.addVectorLayer(filepath + 'fireborder.shp', "fireborderLayer", "ogr")
