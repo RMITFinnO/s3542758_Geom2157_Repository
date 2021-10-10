51eeb144dfcd522b73371573885a3769324f6b18
RMITFinnO <s3542758@student.rmit.edu.au>
2021-10-10 15:10:08 +1100

    Updated to create to files per year
    Took approximately 8 hours of working through this only to find it wasn't working because my QGIS is corrupted - now working on virtual desktop

--- Fresh.py
+++ Fresh.py
@@ -1,6 +1,6 @@
 import processing
 #import variables
-filepath = "D:\\Uni\\2021Semester2\\Geospatial_Programming\\Final_Project_Dataset\\Order_CJPSZB\\mga2020_55\\esrishape\\vicgov_region\\GIPPSLAND-0\\FIRE\\"
+filepath = "H:\\s3542758_FinalProject\\FinalProject_SourceData\\"
 firehistory = "FIRE_HISTORY.shp"
 siteborder = "siteborder.shp"
 
@@ -18,18 +18,14 @@ fireborderLayer.addAttribute(QgsField('SUITABILITY', QVariant.String))
 fireborderLayer.updateFields()
 fireborderLayer.commitChanges()
 
-currentYear = 2021
-extractDict = {'INPUT':fireborderLayer, 'FIELD':'SEASON', 'OPERATOR':0, 'VALUE':str(currentYear), 'OUTPUT':filepath + str(currentYear) + ".shp"}
-Testprocess = processing.run("native:extractbyattribute", extractDict)
-currentYearLayer = iface.addVectorLayer(filepath + str(currentYear) + ".shp", str(currentYear), "ogr")
+#currentYear = 2021
+#extractDict = {'INPUT':fireborderLayer, 'FIELD':'SEASON', 'OPERATOR':0, 'VALUE':str(currentYear), 'OUTPUT':filepath + str(currentYear) + ".shp"}
+#Testprocess = processing.run("native:extractbyattribute", extractDict)
+#currentYearLayer = iface.addVectorLayer(filepath + str(currentYear) + ".shp", str(currentYear), "ogr")
 
 yearList3 = [2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999, 1998, 1997, 1996, 1995, 1994, 1993, 1992, 1991]
-#yearList = ['2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008', '2007', '2006', '2005', '2004', '2003', '2002', '2001', '2000', '1999', '1998', '1997', '1996', '1995', '1994', '1993', '199'2, '1991']
-zones = fireborderLayer
-for zone in zones:
-    
-for year1 in yearList3:
-loopYear = year1
-loopDict = {'INPUT':fireborderLayer, 'FIELD':'SEASON', 'OPERATOR':0, 'VALUE':str(loopYear), 'OUTPUT':filepath + str(loopYear) + ".shp"}
-loopProcess = processing.run("native:extractbyattribute", loopDict)
-print(loopYear)
+ 
+for i in yearList3:
+    loopDict = {'INPUT':fireborderLayer, 'FIELD':'SEASON', 'OPERATOR':0, 'VALUE':str(i), 'OUTPUT':filepath + str(i) + ".shp"}
+    loopProcess = processing.run("native:extractbyattribute", loopDict)
+    loopAddLayer = iface.addVectorLayer(filepath + str(i) + ".shp", "", "ogr")
