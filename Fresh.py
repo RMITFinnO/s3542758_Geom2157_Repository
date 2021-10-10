7a76955703f0ab272140164f11b2b95ec229f87b
RMITFinnO <s3542758@student.rmit.edu.au>
2021-10-10 15:54:01 +1100

    Theorizing difference loop

--- Fresh.py
+++ Fresh.py
@@ -26,6 +26,17 @@ fireborderLayer.commitChanges()
 yearList3 = [2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999, 1998, 1997, 1996, 1995, 1994, 1993, 1992, 1991]
  
 for i in yearList3:
-    loopDict = {'INPUT':fireborderLayer, 'FIELD':'SEASON', 'OPERATOR':0, 'VALUE':str(i), 'OUTPUT':filepath + str(i) + ".shp"}
-    loopProcess = processing.run("native:extractbyattribute", loopDict)
-    loopAddLayer = iface.addVectorLayer(filepath + str(i) + ".shp", "", "ogr")
+    #if i = 2021:
+        #pass
+   # elif i <2021:
+        loopDict = {'INPUT':fireborderLayer, 'FIELD':'SEASON', 'OPERATOR':0, 'VALUE':str(i), 'OUTPUT':filepath + str(i) + ".shp"}
+        loopProcess = processing.run("native:extractbyattribute", loopDict)
+        loopAddLayer = iface.addVectorLayer(filepath + str(i) + ".shp", "", "ogr")
+
+#look into splitvectorlayer 
+total = 2021 layer
+#for i in yearList3:
+    diffDict = {'INPUT':i, 'OVERLAY':total, 'OUTPUT':new}
+    processing.run("native:difference", diffDict)
+    mergeList = [new, total]
+    mergeDict = {'INPUT':mergeList, 'CRS':fireborderLayer, 'OUTPUT':total}
